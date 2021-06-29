e7f5dcff5e0addce6b31b2cff7ac11388a875987
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-06-29 16:14:51 -0600

    mcreate mutations in typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,12 +6,14 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     user: async () => {
-      return User.findAll({});
+      console.log("Lacey is pretty");
+      return User.find();
     },
   },
 
   Mutation: {
     addUser: async (parent, args) => {
+      console.log("Lacey is pretty");
       const user = await User.create(args);
       const token = signToken(user);
 
@@ -19,3 +21,5 @@ const resolvers = {
     },
   },
 };
+
+module.exports = resolvers;
