846e2c2bf1130ff1c7a66b145be9b4181a7ae2d8
Chip Long <chip_long@yahoo.com>
2021-06-29 13:17:38 -0600

    added DB connection

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/roll-for-friendship",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+module.exports = mongoose.connection;
