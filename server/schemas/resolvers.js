2e9d9ff224ee167d4b012d4d8d4aaea5dc35af45
lapape <lapape07@gmail.com>
2021-07-01 17:28:20 -0600

    fixed mequery populate

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,7 +18,9 @@ const resolvers = {
 
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("thoughts");
+        return User.findOne({ _id: context.user._id })
+          .populate("characters")
+          .populate("groups");
       }
       throw new AuthenticationError("You need to be logged in!");
     },
