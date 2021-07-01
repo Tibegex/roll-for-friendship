9a0ff970eff24941b1b2a489e882c6be99f7e2ac
Chip Long <chip_long@yahoo.com>
2021-06-30 19:24:40 -0600

    Fixed auth getUser, removed unnecessary text in login

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -45,9 +45,6 @@ const LoginForm = () => {
           name="email"
           onChange={handleChange}
         />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
       <Form.Group controlId="password">
