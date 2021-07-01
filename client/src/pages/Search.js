2cedbe1b11e911a76f5fcf1433cbe041ca53de6f
Tibegex <joshua_b_lee@yahoo.com>
2021-07-01 14:29:41 -0700

    set up search page and added in basic search bars

---
+++ client/src/pages/Search.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Form, Col, Button } from "react-bootstrap";
+import Auth from "../utils/auth";
+
+const Search = () => {
+  return (
+    <Container>
+      {Auth.loggedIn() ? (
+        <Form.Group>
+          <Form.Row>
+            <Form.Label column="lg" lg={2}>
+              realName
+            </Form.Label>
+            <Col>
+              <Form.Control size="lg" type="text" placeholder="realName" />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column="lg" lg={2}>
+              email
+            </Form.Label>
+            <Col>
+              <Form.Control size="lg" type="text" placeholder="email" />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column="lg" lg={2}>
+              playerLevel
+            </Form.Label>
+            <Col>
+              <Form.Control size="lg" type="text" placeholder="playerLevel" />
+            </Col>
+          </Form.Row>
+          <Button variant="secondary" size="lg" block>
+            Search
+          </Button>
+        </Form.Group>
+      ) : (
+        <>route to login</>
+      )}
+    </Container>
+  );
+};
+
+export default Search;
