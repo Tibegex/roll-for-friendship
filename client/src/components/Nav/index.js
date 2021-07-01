79a2c29965d9449382c637005ef7d5101e43aac4
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-07-01 11:13:38 -0600

    navbar color change

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,12 +3,12 @@ import "./navStyle.css";
 
 const Nav = () => {
   return (
-    <nav class="navbar navbar-light bg-light">
+    <nav id="navbar" class="navbar">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img
             src={process.env.PUBLIC_URL + "RFFLogo.png"}
-            alt=""
+            alt="RFF Logo"
             width="105px"
             height="60px"
             class="d-inline-block align-text-center"
