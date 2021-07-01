2d44da16951145187798245f93a74ea7186b27fe
Chip Long <chip_long@yahoo.com>
2021-07-01 10:07:42 -0600

    fixed login and made homepage respond to login state

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import Auth from "../utils/auth";
 
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
@@ -7,27 +8,31 @@ import SignUpForm from "../components/SignUpForm";
 const Home = () => {
   return (
     <Container>
-      <Row>
-        <Col>
-          <header className="h2">Welcome to Roll for Friendship!</header>
-          <p>This site is meant to group players with game masters.</p>
-          <p>If you are a player, search for a group in your area!</p>
-          <p>
-            If you are a game master, put a group together and be able to
-            provide times for the players to play.
-          </p>
-        </Col>
-        <Col>
-          <Tabs defaultActiveKey="login" id="uncontrolled-tab-example">
-            <Tab eventKey="login" title="Login" key="Login">
-              <LoginForm />
-            </Tab>
-            <Tab eventKey="signUp" title="Sign Up" key="Sign Up">
-              <SignUpForm />
-            </Tab>
-          </Tabs>
-        </Col>
-      </Row>
+      {!Auth.loggedIn() ? (
+        <Row>
+          <Col>
+            <header className="h2">Welcome to Roll for Friendship!</header>
+            <p>This site is meant to group players with game masters.</p>
+            <p>If you are a player, search for a group in your area!</p>
+            <p>
+              If you are a game master, put a group together and be able to
+              provide times for the players to play.
+            </p>
+          </Col>
+          <Col>
+            <Tabs defaultActiveKey="login" id="uncontrolled-tab-example">
+              <Tab eventKey="login" title="Login" key="Login">
+                <LoginForm />
+              </Tab>
+              <Tab eventKey="signUp" title="Sign Up" key="Sign Up">
+                <SignUpForm />
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      ) : (
+        <>You're logged in!</>
+      )}
     </Container>
   );
 };
